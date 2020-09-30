f44f0c8e649339226774215063a74573c785cbdc
josuemunro <josuemunro@gmail.com>
2020-10-01 12:44:03 +1300

    First commit, adding frontend apache server from assignment 1 to test php api connection to trademe

---
+++ Frontend Server/www/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>JosiDrive</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<div class="header">
+  <a href="index.php">
+    <img class="logo" src="logo.png">
+  </a>
+</div>
+<div class="parent">
+<?php
+
+
+?>
+</div>
+
+</body>
+</html>
