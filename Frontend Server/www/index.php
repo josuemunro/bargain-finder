964b3f67ad2433be29890c4959a0a237e7f07da2
josuemunro <josuemunro@gmail.com>
2020-10-01 22:21:22 +1300

    Added a few useful sentences to the README. Battled for a long time but now have an successful connection to the TradeMe sandbox API! Also added php-curl to the provisioning of the vagrant VM. Added custom logo

--- Frontend Server/www/index.php
+++ Frontend Server/www/index.php
@@ -13,8 +13,49 @@
 </div>
 <div class="parent">
 <?php
+$curl = curl_init();
 
+$headr = array();
+$headr[] = 'Content-length: 0';
+$headr[] = 'cache-control: no-cache';
+$headr[] = 'Content-type: application/json';
+// Using this header we get a 500 internal error response
+$headr[] = 'Authorization: OAuth oauth_consumer_key=21037302176B01C359C079657F286791, oauth_token=07D475ED350063E906221BA04897F031, oauth_signature_method=PLAINTEXT, oauth_signature=2106B414378CF13D7B900702BF3BCBA0%2616FF498FE034BADA111DE9445B976D06';
+// Using this header we get a 401 unauthorized response
+// $headr[] = 'Authorization: OAuth wrongauthorization';
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.tmsandbox.co.nz/v1/Search/General.json?searchString=iphone",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_FAILONERROR => true,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => $headr,
+  CURLOPT_SSL_VERIFYPEER => false,
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+if ($response === false)
+{
+    // throw new Exception('Curl error: ' . curl_error($crl));
+    print_r('Curl error: ' . $err);
+}
+curl_close($curl);
+
+echo $response;
+
+$response = json_decode($response, true); //because of true, it's in an array
+
+foreach($response as $List){
+  foreach($List as $value){
+    if ($value['BuyNowPrice'] === 150.0) {
+      echo "Buy Now Price:". $value['BuyNowPrice'];
+    }
+  
+  }
+}
 ?>
 </div>
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# bargain-finder" 
+PHP based web application to be deployed to the AWS cloud using AWS CodeDeploy.
+
+When finished, it should display TradeMe listings via their API.
+The listings displayed will be listings that end at off-peak times so you can get good deals without competition.
